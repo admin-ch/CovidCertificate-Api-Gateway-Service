b236f65c9f18e50ff92f2339f84bbcd9b786c37a
Fabien Cerf <fabien.cerf@bit.admin.ch>
2021-06-01 11:48:01 +0200

    Request Wrapper

---
+++ src/main/java/ch/admin/bag/covidcertificate/gateway/filters/CachedBodyServletInputStream.java
@@ -0,0 +1,41 @@
+package ch.admin.bag.covidcertificate.gateway.filters;
+
+import javax.servlet.ReadListener;
+import javax.servlet.ServletInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class CachedBodyServletInputStream extends ServletInputStream {
+
+    private InputStream cachedBodyInputStream;
+
+    public CachedBodyServletInputStream(byte[] cachedBody) {
+        this.cachedBodyInputStream = new ByteArrayInputStream(cachedBody);
+    }
+
+    @Override
+    public int read() throws IOException {
+        return cachedBodyInputStream.read();
+    }
+
+    @Override
+    public boolean isFinished() {
+        try {
+            return cachedBodyInputStream.available() == 0;
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public boolean isReady() {
+        return true;
+    }
+
+    @Override
+    public void setReadListener(ReadListener readListener) {
+
+    }
+}
