5f49706fa043eb7c61b767fa0384820b78726e47
Yannik Inniger <yannik.inniger@bit.admin.ch>
2021-06-10 17:23:00 +0200

    Remove public modifier

--- src/test/java/ch/admin/bag/covidcertificate/gateway/client/internal/DefaultIdentityAuthorizationClientTest.java
+++ src/test/java/ch/admin/bag/covidcertificate/gateway/client/internal/DefaultIdentityAuthorizationClientTest.java
@@ -20,7 +20,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class DefaultIdentityAuthorizationClientTest {
+class DefaultIdentityAuthorizationClientTest {
     private final JFixture jFixture = new JFixture();
 
     String uuid;
