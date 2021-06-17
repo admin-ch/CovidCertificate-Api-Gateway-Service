bfe729df23feecaae14e41822e257908797e87d6
Mathias Wegmüller <matthias.wegmueller@bit.admin.ch>
2021-06-17 14:41:35 +0200

    VACCINECER-952 Added errors for standardised names in API description for new JSON schema 1.3.0.

--- src/main/java/ch/admin/bag/covidcertificate/gateway/error/ErrorList.java
+++ src/main/java/ch/admin/bag/covidcertificate/gateway/error/ErrorList.java
@@ -59,6 +59,10 @@ public class ErrorList {
             + ERROR_MESSAGE_KEY + INVALID_RECOVERY_INFO_MESSAGE + "}";
     public static final String INVALID_ADDRESS = ERROR_CODE_KEY + 474 + ","
             + ERROR_MESSAGE_KEY + "Paper-based delivery requires a valid address.}";
+    public static final String INVALID_STANDARDISED_GIVEN_NAME = ERROR_CODE_KEY + 475 + ","
+            + ERROR_MESSAGE_KEY + "Invalid given name! The standardised given name exceeds 80 chars}";
+    public static final String INVALID_STANDARDISED_FAMILY_NAME = ERROR_CODE_KEY + 476 + ","
+            + ERROR_MESSAGE_KEY + "Invalid family name! The standardised family name exceeds 80 chars}";
 
     // Authorization
     private static final int INVALID_SIGNATURE_CODE = 490;
