33a83817b6a14757c6813b457725183b2888f139
Yannik Inniger <yannik.inniger@gmail.com>
2021-06-24 12:29:06 +0200

    Add return parameter for appDeliveryError

--- src/main/java/ch/admin/bag/covidcertificate/gateway/service/dto/incoming/CovidCertificateCreateResponseDto.java
+++ src/main/java/ch/admin/bag/covidcertificate/gateway/service/dto/incoming/CovidCertificateCreateResponseDto.java
@@ -1,7 +1,12 @@
 package ch.admin.bag.covidcertificate.gateway.service.dto.incoming;
 
+import ch.admin.bag.covidcertificate.gateway.error.RestError;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
+
+import static ch.admin.bag.covidcertificate.gateway.error.ErrorList.*;
 
 @Getter
 @ToString
@@ -13,4 +18,7 @@ public class CovidCertificateCreateResponseDto {
     @Schema(type = "string", format = "byte")
     private byte[] qrCode;
     private String uvci;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @Schema(required = false)
+    private RestError appDeliveryError;
 }
