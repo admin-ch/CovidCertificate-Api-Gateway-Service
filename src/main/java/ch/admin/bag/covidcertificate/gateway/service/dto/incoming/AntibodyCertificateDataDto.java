caf4a7073b814f9f5c2c96e6c0cc9ab111a26dd7
mofobo <mohammed.fonsecaboukhalat@bit.admin.ch>
2021-11-04 13:23:43 +0100

    correct typo

--- src/main/java/ch/admin/bag/covidcertificate/gateway/service/dto/incoming/AntibodyCertificateDataDto.java
+++ src/main/java/ch/admin/bag/covidcertificate/gateway/service/dto/incoming/AntibodyCertificateDataDto.java
@@ -13,5 +13,5 @@ public class AntibodyCertificateDataDto {
     @Schema(example = "2021-10-03", description = "date when the sample for the test was collected that led to serology positive test obtained through a procedure established by a public health authority. Format: ISO 8601 date without time.")
     private LocalDate sampleDate;
     @Schema(example = "Walk-in-Lyss AG", description = "name of centre or facility. Format: string, maxLength: 80 CHAR.")
-    private String testingCentreOrFacility;
+    private String testingCenterOrFacility;
 }
