4cc5bd78a0f6d8171a7c1be823857f34530b7452
mofobo <mohammed.fonsecaboukhalat@bit.admin.ch>
2022-01-26 17:29:52 +0100

    removed unused 'code' property

--- src/main/java/ch/admin/bag/covidcertificate/gateway/domain/Issuable.java
+++ src/main/java/ch/admin/bag/covidcertificate/gateway/domain/Issuable.java
@@ -1,22 +1,8 @@
 package ch.admin.bag.covidcertificate.gateway.domain;
 
 public enum Issuable {
-
-    CH_AND_ABROAD("ch_and_abroad"),
-    CH_ONLY("ch_only"),
-    ABROAD_ONLY("abroad_only"),
-    UNDEFINED("undefined");
-
-    /**
-     * The code used to store the value.
-     */
-    private String code;
-
-    Issuable(String code) {
-        this.code = code;
-    }
-
-    public String getCode() {
-        return this.code;
-    }
+    CH_AND_ABROAD,
+    CH_ONLY,
+    ABROAD_ONLY,
+    UNDEFINED
 }
