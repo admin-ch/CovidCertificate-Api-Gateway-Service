4f7443d4a3b00da33589c6d443b60a3dd0dd5ec8
Harald Lösing <harald.loesing@bit.admin.ch>
2023-01-04 11:54:35 +0100

    Brought back AllArgsConstructor as test needs it - murks.

--- src/main/java/ch/admin/bag/covidcertificate/gateway/service/dto/incoming/RapidTestDto.java
+++ src/main/java/ch/admin/bag/covidcertificate/gateway/service/dto/incoming/RapidTestDto.java
@@ -2,6 +2,7 @@ package ch.admin.bag.covidcertificate.gateway.service.dto.incoming;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -9,6 +10,7 @@ import lombok.ToString;
 @Getter
 @ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class RapidTestDto extends IssuableRapidTestDto {
     @Schema(type = "boolean", example = "true", description = "If rapid test is accepted by the BAG and the EU.")
     private Boolean active;
